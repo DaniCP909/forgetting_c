04f8ade66377273403974c8285b70bba2cb15e90
DaniCP909 <d.corredorpadrino@gmail.com>
2023-09-06 07:37:12 +0200

    array elementary use of arrays

--- arrays/step_0/src/array00.c
+++ arrays/step_0/src/array00.c
@@ -1,7 +1,28 @@
+/*
+
+Basic use of arrays *for me*
+
+*/
+
 #include <stdio.h>
 
 int main()
 {
-	printf("Lumos\n");
+	
+	int x1 = 33;
+	
+	int * p_x1;
+	p_x1 = &x1;
+	
+	int ** pp_x1;
+	pp_x1 = &p_x1;
+	
+	printf("x1   = %d \n", x1);
+	printf("p_x1 = %p \n",p_x1);
+	printf("p_x1 points value: %d (of x1) \n", *p_x1);
+	printf("p_x1 dir: %p (&) \n", &p_x1);
+	printf("x1 dir %p (&) \n", &x1);
+	printf("pp_x1 = dir(p_x1) = %p \n",pp_x1);
+	
 	return 0;
 }
